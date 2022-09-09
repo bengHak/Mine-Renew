546695fc5cf1e66a12c5912ed6055ebcbe44f3f7
고병학 <qkwl4678@naver.com>
2022-09-09 14:11:29 +0900

    init

---
+++ Mine-Renew/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mine-Renew
+//
+//  Created by 고병학 on 2022/09/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
