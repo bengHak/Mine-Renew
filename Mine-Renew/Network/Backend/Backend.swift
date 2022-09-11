f16b0626985139c781bc196a67150b322e01726e
고병학 <qkwl4678@naver.com>
2022-09-11 21:59:12 +0900

    feat: add amplify package

---
+++ Mine-Renew/Network/Backend/Backend.swift
@@ -0,0 +1,24 @@
+//
+//  Backend.swift
+//  Mine-Renew
+//
+//  Created by 고병학 on 2022/09/11.
+//
+import UIKit
+import Amplify
+
+class Backend {
+    static let shared = Backend()
+    static func initialize() -> Backend {
+        return .shared
+    }
+    private init() {
+      // initialize amplify
+      do {
+        try Amplify.configure()
+        print("Initialized Amplify");
+      } catch {
+        print("Could not initialize Amplify: \(error)")
+      }
+    }
+}
